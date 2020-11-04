ec31c4c74e3d8430a317ad02af80971763881449
pomm0 <gruber.m94@gmail.com>
2020-11-04 14:31:22 +0100

    Use component

--- app/components/map.js
+++ app/components/map.js
@@ -1,14 +1,15 @@
 import Component from '@glimmer/component';
 import Mapboxgl from 'mapbox-gl';
-import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
 
 export default class MapComponent extends Component {
   initilizeMap() {
+    Mapboxgl.accessToken = '';  // Put your accessToken here
     const map = new Mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/satellite-v9',
-      center: [0, 0],
-      zoom: 19
+      center: [12, 50],
+      zoom: 5
     });
     const Draw = new MapboxDraw();
     map.addControl(Draw);
